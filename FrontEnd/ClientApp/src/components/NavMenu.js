a188a370da44fc9f42efdf05206abcb65838dbd9
erimo832@gmail.com <erimo832@gmail.com>
2020-01-10 16:59:05 +0100

    link back to orbitibro page from navbar brand

--- FrontEnd/ClientApp/src/components/NavMenu.js
+++ FrontEnd/ClientApp/src/components/NavMenu.js
@@ -27,7 +27,8 @@ export class NavMenu extends Component {
       <header>        
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">orbiTibro              
+            <NavbarBrand>
+                <NavLink className="text-dark" href="http://www.orbitibro.se/">orbiTibro</NavLink>
             </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
